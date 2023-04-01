7a180758adfce6585ca090cd3562c051b768458b
rodfer0x80 <rodfer0x80@proton.me>
2023-04-01 02:06:51 +0100

    .

--- src/pandemic/Agent.java
+++ src/pandemic/Agent.java
@@ -1,6 +1,7 @@
 package pandemic;
 
-// minimax search tree
+// montecarlo search tree
 // show user rules, own cards, deck probabilities, possible next turn actions
 // interact with user to select game plans and show probabilities of 4 best next moves
 // along with next actions that need to be taken for the next 4 turns
+
